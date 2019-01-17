b7bff6670f6cd083774b53e165f0758bb85e6f2a
Ricardo Tubio Pardavila <rtpardavila@gmail.com>
2019-01-17 22:11:32 +0900

    [master@20190117:221132] Included right docker run syntax in server.sh

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -t -i -v --name open-elevation $(pwd)/data:/code/data -p 8001:8080 openelevation/open-elevation
+docker build -f config/Dockerfile -t open-elevation .
+
+docker run -t -i -d --name open-elevation -v $(pwd)/data:/code/data -p 8001:8080 open-elevation
